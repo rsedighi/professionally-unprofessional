388c10d6d6147c67a9769bd32d8cc239693e4a6f
Ramin <rsedighi@gmail.com>
2014-05-24 00:44:33 -0700

    can login via google

--- Gemfile
+++ Gemfile
@@ -12,3 +12,6 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'devise'
+gem "omniauth-google-oauth2"
+gem 'figaro'
+gem 'binding_of_caller'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519190605) do
+ActiveRecord::Schema.define(version: 20140523200859) do
 
   create_table "jobs", force: true do |t|
     t.string   "company_name"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20140519190605) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
